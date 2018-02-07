34e6687e8c28b4b75f19bb5ec004ed0ee8d0cb13
zhihaofans <zhihaofans@hotmail.com>
2018-02-08 00:12:50 +0800

    Unfinish update

--- app/src/main/java/com/zhihaofans/androidbox/view/MainActivity.kt
+++ app/src/main/java/com/zhihaofans/androidbox/view/MainActivity.kt
@@ -1,40 +1,102 @@
 package com.zhihaofans.androidbox.view
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
-import android.support.design.widget.CoordinatorLayout
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment.getExternalStorageDirectory
+import android.provider.Settings
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
-import com.jyuesong.android.kotlin.extract._browse
-import com.jyuesong.android.kotlin.extract._sendSMS
-import com.jyuesong.android.kotlin.extract._toast
 import com.orhanobut.logger.Logger
+import com.tbruyelle.rxpermissions2.RxPermissions
+import com.wx.android.common.util.FileUtils
+import com.zhihaofans.androidbox.R
 import com.zhihaofans.androidbox.mod.QrcodeMod
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
-import org.jetbrains.anko.share
-import org.jetbrains.anko.startActivity
-import com.zhihaofans.androidbox.R
-import org.jetbrains.anko.browse
-import org.jetbrains.anko.selector
 
 
 class MainActivity : AppCompatActivity() {
     private val qrcode = QrcodeMod()
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toolbar_main.subtitle = "v0.0.1"
         setSupportActionBar(toolbar_main)
+        val rxPermissions = RxPermissions(this)
         qrcode.setActivity(this@MainActivity)
         toolbar_main.setOnMenuItemClickListener { item ->
             when (item.itemId) {
                 R.id.menu_setting -> startActivity<SettingActivity>()
             }
             true
+        }
+        button_dotnomedia.onClick {
+            rxPermissions
+                    .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .subscribe({ granted ->
+                        if (granted) {
+                            alert {
+                                customView {
+                                    verticalLayout {
+                                        textView("路径:")
+                                        var path = editText {
+                                            setText("${getExternalStorageDirectory().absolutePath}/")
+                                        }.text.toString()
+                                        yesButton {
+                                            if (path.isNotEmpty()) {
+                                                if (!path.endsWith("/")) path += "/"
+                                                if (FileUtils.isFileExist(path)) {
+                                                    Snackbar.make(coordinatorLayout_main, "该路径是个文件不是文件夹，生成文件失败",
+                                                            Snackbar.LENGTH_SHORT).show()
+                                                } else {
+                                                    if (FileUtils.isFolderExist(path)) {
+                                                        val fileName = path + ".nomedia"
+                                                        if (FileUtils.isFileExist(fileName)) {
+                                                            Snackbar.make(coordinatorLayout_main, "文件已存在，不需要生成",
+                                                                    Snackbar.LENGTH_SHORT).show()
+                                                        } else {
+                                                            if (FileUtils.writeFile(fileName, "")) {
+                                                                Snackbar.make(coordinatorLayout_main, "生成文件成功",
+                                                                        Snackbar.LENGTH_SHORT).show()
+                                                            } else {
+                                                                Snackbar.make(coordinatorLayout_main, "生成文件失败",
+                                                                        Snackbar.LENGTH_SHORT).show()
+                                                            }
+                                                        }
+                                                    } else {
+                                                        Snackbar.make(coordinatorLayout_main, "该文件夹不存在，要先创建吗？",
+                                                                Snackbar.LENGTH_LONG)
+                                                                .setAction("创建并生成文件", {
+
+                                                                }).show()
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        noButton { }
+                                    }
+                                }
+                            }.show()
+                        } else {
+                            Snackbar.make(coordinatorLayout_main, "生成文件失败，没有储存权限。", Snackbar.LENGTH_SHORT)
+                                    .setAction("授权", {
+                                        val intent = Intent()
+                                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                                        intent.data = Uri.fromParts("package", packageName, null)
+                                        startActivity(intent)
+                                    })
+                                    .show()
+                        }
+                    })
+
         }
         button_qrcode.onClick {
             val qrcodePlugin = qrcode.getInstalledPlugin(this@MainActivity)

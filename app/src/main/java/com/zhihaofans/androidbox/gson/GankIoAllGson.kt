072167f4c15507fbfc41596349db7b5405170626
zhihaofans <zhihaofans@hotmail.com>
2018-03-14 00:15:06 +0800

    Update NewsBoxActivity(gank.io)

---
+++ app/src/main/java/com/zhihaofans/androidbox/gson/GankIoAllGson.kt
@@ -0,0 +1,9 @@
+package com.zhihaofans.androidbox.gson
+
+/**
+ * Created by zhihaofans on 2018/3/13.
+ */
+data class GankIoAllGson(
+        val error: Boolean,
+        val results: MutableList<GankIoAllListGson>
+)
--- app/src/main/java/com/zhihaofans/androidbox/mod/NewsBoxMod.kt
+++ app/src/main/java/com/zhihaofans/androidbox/mod/NewsBoxMod.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.google.gson.Gson
 import com.zhihaofans.androidbox.R
 import com.zhihaofans.androidbox.gson.DgtleIndexGson
+import com.zhihaofans.androidbox.gson.GankIoAllGson
 import com.zhihaofans.androidbox.gson.SspaiArticleGson
 
 /**
@@ -14,9 +15,10 @@ class NewsBoxMod {
     private val g = Gson()
     fun sites(): MutableList<List<String>> {
         return mutableListOf(
-                listOf("dgtle_news", nowContext!!.getString(R.string.text_site_dgtle_news)),
-                listOf("dgtle_forum", nowContext!!.getString(R.string.text_site_dgtle_forum)),
-                listOf("sspai_article", nowContext!!.getString(R.string.text_site_sspai_article))
+                listOf("dgtle_news", getString(R.string.text_site_dgtle_news)),
+                listOf("dgtle_forum", getString(R.string.text_site_dgtle_forum)),
+                listOf("sspai_article", getString(R.string.text_site_sspai_article)),
+                listOf("gank_io_all", getString(R.string.text_site_gank_io))
         )
     }
 
@@ -35,7 +37,7 @@ class NewsBoxMod {
                 thisSiteInfo["has_headers"] = true
                 thisSiteInfo["headers"] = mutableMapOf(
                         Pair("content-type", "application/json, text/javascript, */*; q=0.01"),
-                        Pair("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36")
+                        Pair("user-agent", getString(R.string.setting_http_defaultua))
                 )
             }
             "dgtle_forum" -> {
@@ -44,16 +46,24 @@ class NewsBoxMod {
                 thisSiteInfo["has_headers"] = true
                 thisSiteInfo["headers"] = mutableMapOf(
                         Pair("content-type", "application/json, text/javascript, */*; q=0.01"),
-                        Pair("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36")
+                        Pair("user-agent", getString(R.string.setting_http_defaultua))
                 )
             }
             "sspai_article" -> {
-                thisSiteInfo["api_url"] = "https://sspai.com/api/v1/articles?offset={pageMinus|1}00&limit=100&type=recommend_to_home&sort=recommend_to_home_at&include_total=false"
+                thisSiteInfo["api_url"] = "https://sspai.com/api/v1/articles?offset={pageMinus|1}0&limit=10&type=recommend_to_home&sort=recommend_to_home_at&include_total=false"
                 thisSiteInfo["news_item_web"] = "http://www.dgtle.com/thread-{news_item_web_id}-1-1.html"
                 thisSiteInfo["has_headers"] = true
                 thisSiteInfo["headers"] = mutableMapOf(
                         Pair("content-type", "application/json, text/javascript, */*; q=0.01"),
-                        Pair("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36")
+                        Pair("user-agent", getString(R.string.setting_http_defaultua))
+                )
+            }
+            "gank_io_all" -> {
+                thisSiteInfo["api_url"] = "http://gank.io/api/data/all/20/{page}"
+                thisSiteInfo["news_item_web"] = "{news_item_web_id}"
+                thisSiteInfo["has_headers"] = true
+                thisSiteInfo["headers"] = mutableMapOf(
+                        Pair("user-agent", getString(R.string.setting_http_defaultua))
                 )
             }
             else -> thisSiteInfo["yes"] = false
@@ -76,17 +86,35 @@ class NewsBoxMod {
                 }
             }
             "sspai_article" -> {
-                val dgtleIndex = g.fromJson(siteHtmlString, SspaiArticleGson::class.java)
-                val dgtleListIndex = dgtleIndex.list
-                returnList["title"] = dgtleListIndex.map {
+                val sspaiIndex = g.fromJson(siteHtmlString, SspaiArticleGson::class.java)
+                val sspaiListIndex = sspaiIndex.list
+                returnList["title"] = sspaiListIndex.map {
                     it.title
                 }
-                returnList["web_url"] = dgtleListIndex.map {
+                returnList["web_url"] = sspaiListIndex.map {
                     "https://www.sspai.com/post/${it.id}"
+                }
+            }
+            "gank_io_all" -> {
+                val gankIndex = g.fromJson(siteHtmlString, GankIoAllGson::class.java)
+                val gankListIndex = gankIndex.results
+                if (gankIndex.error) {
+                    return returnList
+                }
+                returnList["title"] = gankListIndex.map {
+                    it.desc
+                }
+                returnList["web_url"] = gankListIndex.map {
+                    it.url
                 }
             }
         }
         return returnList
+    }
+
+    fun getString(id: Int): String {
+        if (nowContext == null) return ""
+        return nowContext!!.getString(id)
     }
 
     fun pageRuleParser(oldUrl: String, page: Int): String {

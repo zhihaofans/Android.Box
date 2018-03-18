fbba0b21ca5141e3d5005962c5cb0ec419acfe3e
zhihaofans <zhihaofans@hotmail.com>
2018-03-19 01:17:26 +0800

    Update AppManagementActivity

--- app/src/main/java/com/zhihaofans/androidbox/view/AppManagementActivity.kt
+++ app/src/main/java/com/zhihaofans/androidbox/view/AppManagementActivity.kt
@@ -1,5 +1,6 @@
 package com.zhihaofans.androidbox.view
 
+import android.Manifest
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
 import android.graphics.drawable.Drawable
@@ -8,11 +9,15 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.widget.ListView
 import com.orhanobut.logger.Logger
+import com.wx.android.common.util.ClipboardUtils
 import com.wx.android.common.util.FileUtils
 import com.zhihaofans.androidbox.R
 import com.zhihaofans.androidbox.adapter.ListViewAdapter
 import kotlinx.android.synthetic.main.activity_app_management.*
 import kotlinx.android.synthetic.main.content_app_management.*
+import me.weyye.hipermission.HiPermission
+import me.weyye.hipermission.PermissionCallback
+import me.weyye.hipermission.PermissionItem
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onItemClick
 import java.text.SimpleDateFormat
@@ -96,42 +101,80 @@ class AppManagementActivity : AppCompatActivity() {
                         okButton { }
                     }.show()*/
 
-                    val act_app = listOf(getStr(R.string.text_app_info), getStr(R.string.text_app_apk))
+                    val act_app = listOf(getStr(R.string.text_app_info), getStr(R.string.text_app_apk), getStr(R.string.text_icon))
                     selector(childItem["appName"] as String, act_app, { _, i ->
                         when (i) {
                             0 -> {
+                                //app info
                                 val act_appInfo = listOf(
                                         "${getStr(R.string.text_app_name)}:$thisAppName",
                                         "${getStr(R.string.text_app_packagename)}:$thisAppPackageName",
                                         "${getStr(R.string.text_app_version)}:$thisAppVersionName ($thisAppVersionCode)",
                                         "${getStr(R.string.text_app_apkpath)}:$thisApkPath",
-                                        "${getStr(R.string.text_app_size)}:$thisApkSize B",
+                                        "${getStr(R.string.text_app_size)}:${fileSize2String(thisApkSize)}",
                                         "${getStr(R.string.text_app_firstinstalltime)}:$thisAppFirstInstallTime",
                                         "${getStr(R.string.text_app_lastupdatetime)}:$thisAppLastUpdateTime"
                                 )
+                                Logger.d(act_appInfo)
                                 //TODO:应用选项
                                 selector(getStr(R.string.text_app_info), act_appInfo, { _, ii ->
-                                    when (ii) {
-                                        0 -> {
-                                            alert {
-                                                customView {
-                                                    verticalLayout {
-                                                        val input: String = editText(act_appInfo[ii]).text.toString()
-                                                        neutralPressed(R.string.text_share, {
-                                                            toast(input)
-                                                        })
-                                                        negativeButton(R.string.text_cancel, {
+                                    alert {
+                                        customView {
+                                            verticalLayout {
+                                                val input: String = editText(act_appInfo[ii]).text.toString()
+                                                neutralPressed(R.string.text_share, {
+                                                    share(input)
+                                                })
+                                                negativeButton(R.string.text_cancel, {
 
-                                                        })
-                                                        positiveButton(R.string.text_copy, {
-
-                                                        })
-                                                    }
-                                                }
+                                                })
+                                                positiveButton(R.string.text_copy, {
+                                                    ClipboardUtils.copy(this@AppManagementActivity, input)
+                                                    Snackbar.make(coordinatorLayout_app, R.string.text_ok, Snackbar.LENGTH_SHORT).show()
+                                                })
                                             }
                                         }
-                                    }
+                                    }.show()
                                 })
+                            }
+
+                            1 -> {
+                                //apk file
+                                /*
+                                val act_apk = listOf(getStr(R.string.text_app_info), getStr(R.string.text_app_apk), getStr(R.string.text_icon))
+                                selector(childItem["appName"] as String, act_app, { _, ii ->
+
+                                })
+*/
+                                val permissionItems = ArrayList<PermissionItem>()
+                                permissionItems.add(PermissionItem(Manifest.permission.WRITE_EXTERNAL_STORAGE, getString(R.string.text_permission_storage), R.drawable.permission_ic_storage))
+                                HiPermission.create(this@AppManagementActivity)
+                                        .permissions(permissionItems)
+                                        .checkMutiPermission(object : PermissionCallback {
+                                            override fun onClose() {
+                                                Logger.i("onClose")
+                                                Snackbar.make(coordinatorLayout_app, "用户关闭权限申请", Snackbar.LENGTH_SHORT)
+                                            }
+
+                                            override fun onFinish() {
+                                                FileUtils.copyFile(thisApkPath, )
+                                            }
+
+                                            override fun onDeny(permission: String, position: Int) {
+                                                Logger.d("onDeny")
+                                                toast("权限申请失败")
+                                                Snackbar.make(coordinatorLayout_app, "权限申请失败", Snackbar.LENGTH_SHORT)
+                                            }
+
+                                            override fun onGuarantee(permission: String, position: Int) {
+                                                Logger.d("onGuarantee")
+                                                Snackbar.make(coordinatorLayout_app, "Error:onGuarantee($position)", Snackbar.LENGTH_SHORT)
+                                            }
+                                        })
+                            }
+
+                            2 -> {
+                                //app icon
                             }
                         }
                     })
@@ -153,7 +196,18 @@ class AppManagementActivity : AppCompatActivity() {
     }
 
     private fun time2date(time: Long): String {
-        return SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.CHINA).format(Date(time * 1000)) as String
+        return SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.CHINA).format(Date(time)) as String
     }
 
+    private fun fileSize2String(fs: Int): String {
+        var result = fs.toFloat()
+        var times = 0
+        while (result >= 1024) {
+            result /= 1024
+            times++
+        }
+        val units = mutableListOf("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB", "BB", "NB", "DB", "CB")
+        val sizeUnit = if (times >= units.size) "???" else units[times]
+        return "$result $sizeUnit"
+    }
 }

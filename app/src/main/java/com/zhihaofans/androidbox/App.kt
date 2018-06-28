573ecbb395a40a16912a114d721efe81c50c9619
zhihaofans <zhihaofans@hotmail.com>
2018-06-28 17:51:26 +0800

    Add SpiderMan to show crash info

--- app/src/main/java/com/zhihaofans/androidbox/App.kt
+++ app/src/main/java/com/zhihaofans/androidbox/App.kt
@@ -5,6 +5,7 @@ import com.facebook.drawee.backends.pipeline.Fresco
 import com.liulishuo.filedownloader.FileDownloader
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
+import com.simple.spiderman.SpiderMan
 
 
 /**
@@ -18,5 +19,15 @@ class App : Application() {
         Logger.addLogAdapter(AndroidLogAdapter())
         FileDownloader.setupOnApplicationOnCreate(this)
         Fresco.initialize(this)
+        SpiderMan.getInstance()
+                .init(this)
+                //设置是否捕获异常，不弹出崩溃框
+                .setEnable(true)
+                //设置是否显示崩溃信息展示页面
+                .showCrashMessage(true)
+                //是否回调异常信息，友盟等第三方崩溃信息收集平台会用到,
+                .setOnCrashListener { t, ex, model ->
+                    //CrashModel 崩溃信息记录，包含设备信息
+                }
     }
 }

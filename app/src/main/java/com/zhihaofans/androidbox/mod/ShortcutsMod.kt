4645ea1a0517991657632d5c76f3aad6932db14e
zhihaofans <zhihaofans@hotmail.com>
2018-09-11 15:33:01 +0800

    App Shortcuts: Newsbox => Feed; Update to v0.0.58

--- app/src/main/java/com/zhihaofans/androidbox/mod/ShortcutsMod.kt
+++ app/src/main/java/com/zhihaofans/androidbox/mod/ShortcutsMod.kt
@@ -2,7 +2,7 @@ package com.zhihaofans.androidbox.mod
 
 import android.app.Activity
 import android.os.Bundle
-import com.zhihaofans.androidbox.view.NewsBoxActivity
+import com.zhihaofans.androidbox.view.FeedActivity
 import com.zhihaofans.androidbox.view.QrcodeActivity
 import com.zhihaofans.androidbox.view.ServerChanActivity
 import org.jetbrains.anko.startActivity
@@ -26,11 +26,11 @@ class ServerChanShortcuts : Activity() {
     }
 }
 
-class NewsboxShortcuts : Activity() {
+class FeedShortcuts : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //SharedPreferencesUtils.init(this)
-        startActivity<NewsBoxActivity>()
+        startActivity<FeedActivity>()
         finish()
     }
 

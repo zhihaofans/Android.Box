eb6c4332da869d6d3195e8d15531c98f66963a55
zhihaofans <zhihaofans@hotmail.com>
2018-12-09 23:52:07 +0800

    Update XXDown

--- app/src/main/java/com/zhihaofans/androidbox/view/XXDownActivity.kt
+++ app/src/main/java/com/zhihaofans/androidbox/view/XXDownActivity.kt
@@ -1,14 +1,13 @@
 package com.zhihaofans.androidbox.view
 
+import android.app.ProgressDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.zhihaofans.androidbox.R
 import com.zhihaofans.androidbox.data.XXDownResultData
 import com.zhihaofans.androidbox.data.XXDownResultUrlData
-import com.zhihaofans.androidbox.kotlinEx.init
-import com.zhihaofans.androidbox.kotlinEx.removeAllItems
-import com.zhihaofans.androidbox.kotlinEx.snackbar
+import com.zhihaofans.androidbox.kotlinEx.*
 import com.zhihaofans.androidbox.mod.ItemNameMod
 import com.zhihaofans.androidbox.mod.UrlMod
 import com.zhihaofans.androidbox.mod.XXDownMod
@@ -32,6 +31,37 @@ class XXDownActivity : AppCompatActivity() {
                 when (menuI) {
                     0 -> {
                         //TODO:自动识别
+                        alert {
+                            customView {
+                                val inputUrl = editText()
+                                okButton {
+                                    when {
+                                        inputUrl.string().isEmpty() -> {
+                                            coordinatorLayout_xxdown.snackbar("请输入内容")
+                                        }
+                                        inputUrl.string().isUrl() -> {
+                                            val loadingProgressBar = indeterminateProgressDialog(message = "Please wait a bit…", title = "Loading...")
+                                            loadingProgressBar.setCancelable(false)
+                                            loadingProgressBar.setCanceledOnTouchOutside(false)
+                                            loadingProgressBar.show()
+                                            doAsync {
+                                                val result = auto(inputUrl.string())
+                                                uiThread {
+                                                    if (result == null) {
+                                                        coordinatorLayout_xxdown.snackbar("解析失败，不支持该网址")
+                                                    } else {
+                                                        initListView(result, loadingProgressBar)
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        else -> {
+                                            coordinatorLayout_xxdown.snackbar("请输入正确的网址")
+                                        }
+                                    }
+                                }
+                            }
+                        }.show()
                     }
                     1 -> {
                         val defaultUrlList = listOf(
@@ -58,25 +88,7 @@ class XXDownActivity : AppCompatActivity() {
                                                 doAsync {
                                                     val xxDownResultData = this@XXDownActivity.auto(input)
                                                     uiThread {
-                                                        if (xxDownResultData == null) {
-                                                            loadingProgressBar.dismiss()
-                                                            snackbar(coordinatorLayout_xxdown, "错误：返回结果为NULL")
-                                                        } else {
-                                                            if (xxDownResultData.success) {
-                                                                resultList.clear()
-                                                                listView_xxdown.removeAllItems()
-                                                                resultList = xxDownResultData.url.toMutableList()
-                                                                listView_xxdown.init(this@XXDownActivity, resultList.map { i -> i.url })
-                                                                listView_xxdown.setOnItemClickListener { _, _, pos, _ ->
-                                                                    SystemUtil.browse(this@XXDownActivity, resultList[pos].url)
-                                                                }
-                                                                loadingProgressBar.dismiss()
-                                                                snackbar(coordinatorLayout_xxdown, "加载完毕，共${resultList.size}个结果")
-                                                            } else {
-                                                                loadingProgressBar.dismiss()
-                                                                snackbar(coordinatorLayout_xxdown, "错误：${xxDownResultData.message}")
-                                                            }
-                                                        }
+                                                        initListView(xxDownResultData, loadingProgressBar)
                                                     }
                                                 }
                                             }
@@ -101,6 +113,28 @@ class XXDownActivity : AppCompatActivity() {
                 XXDownMod.get(ItemNameMod.XXDOWN_SITE_BILIBILI_VIDEO_THUMBNAIL, url)
             }
             else -> null
+        }
+    }
+
+    private fun initListView(data: XXDownResultData?, loadingProgressDialog: ProgressDialog) {
+        if (data == null) {
+            loadingProgressDialog.dismiss()
+            snackbar(coordinatorLayout_xxdown, "错误：返回结果为NULL")
+        } else {
+            if (data.success) {
+                resultList.clear()
+                listView_xxdown.removeAllItems()
+                resultList = data.url.toMutableList()
+                listView_xxdown.init(this@XXDownActivity, resultList.map { i -> i.url })
+                listView_xxdown.setOnItemClickListener { _, _, pos, _ ->
+                    SystemUtil.browse(this@XXDownActivity, resultList[pos].url)
+                }
+                loadingProgressDialog.dismiss()
+                snackbar(coordinatorLayout_xxdown, "加载完毕，共${resultList.size}个结果")
+            } else {
+                loadingProgressDialog.dismiss()
+                snackbar(coordinatorLayout_xxdown, "错误：${data.message}")
+            }
         }
     }
 }

bbd0e5f5147c613e92c16c0a92a8d531642fcb25
zhihaofans <zhihaofans@hotmail.com>
2019-02-27 20:15:24 +0800

    Update Share2SaveActivity, Update to v0.0.91

--- app/src/main/java/com/zhihaofans/androidbox/util/ToastUtil.kt
+++ app/src/main/java/com/zhihaofans/androidbox/util/ToastUtil.kt
@@ -1,6 +1,7 @@
 package com.zhihaofans.androidbox.util
 
 import android.widget.Toast
+import dev.utils.app.AppUtils
 import dev.utils.app.toast.ToastTintUtils
 
 /**
@@ -18,10 +19,22 @@ class ToastUtil {
         fun normal(text: String) = ToastTintUtils.normal(text)
         fun warning(text: String) = ToastTintUtils.warning(text)
 
+        fun success(text: Int) = ToastTintUtils.success(AppUtils.getString(text))
+        fun error(text: Int) = ToastTintUtils.error(AppUtils.getString(text))
+        fun info(text: Int) = ToastTintUtils.info(AppUtils.getString(text))
+        fun normal(text: Int) = ToastTintUtils.normal(AppUtils.getString(text))
+        fun warning(text: Int) = ToastTintUtils.warning(AppUtils.getString(text))
+
         fun successLong(text: String) = ToastTintUtils.success(text, Toast.LENGTH_LONG)
         fun errorLong(text: String) = ToastTintUtils.error(text, Toast.LENGTH_LONG)
         fun infoLong(text: String) = ToastTintUtils.info(text, Toast.LENGTH_LONG)
         fun normalLong(text: String) = ToastTintUtils.normal(text, Toast.LENGTH_LONG)
         fun warningLong(text: String) = ToastTintUtils.warning(text, Toast.LENGTH_LONG)
+
+        fun successLong(text: Int) = ToastTintUtils.success(AppUtils.getString(text), Toast.LENGTH_LONG)
+        fun errorLong(text: Int) = ToastTintUtils.error(AppUtils.getString(text), Toast.LENGTH_LONG)
+        fun infoLong(text: Int) = ToastTintUtils.info(AppUtils.getString(text), Toast.LENGTH_LONG)
+        fun normalLong(text: Int) = ToastTintUtils.normal(AppUtils.getString(text), Toast.LENGTH_LONG)
+        fun warningLong(text: Int) = ToastTintUtils.warning(AppUtils.getString(text), Toast.LENGTH_LONG)
     }
 }
--- app/src/main/java/com/zhihaofans/androidbox/view/Share2SaveActivity.kt
+++ app/src/main/java/com/zhihaofans/androidbox/view/Share2SaveActivity.kt
@@ -2,20 +2,24 @@ package com.zhihaofans.androidbox.view
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import com.orhanobut.logger.Logger
 import com.xuexiang.xui.XUI
 import com.xuexiang.xui.widget.dialog.LoadingDialog
 import com.zhihaofans.androidbox.R
+import com.zhihaofans.androidbox.kotlinEx.hasNotChild
 import com.zhihaofans.androidbox.kotlinEx.isActionSend
+import com.zhihaofans.androidbox.kotlinEx.remove
 import com.zhihaofans.androidbox.kotlinEx.startsWithList
-import com.zhihaofans.androidbox.util.FileUtil
-import com.zhihaofans.androidbox.util.IntentUtil
-import com.zhihaofans.androidbox.util.XUIUtil
+import com.zhihaofans.androidbox.util.*
 import dev.utils.app.UriUtils
+import dev.utils.app.image.BitmapUtils
 import dev.utils.app.toast.ToastTintUtils
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 
@@ -28,35 +32,65 @@ import org.jetbrains.anko.uiThread
  */
 
 class Share2SaveActivity : Activity() {
-    private var saveText: String? = null
     private val xuiUtil = XUIUtil(this)
     private var loadingDialog: LoadingDialog? = null
+    private var mIntent: Intent? = null
+    private var mimeType = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         XUI.initTheme(this)
         super.onCreate(savedInstanceState)
-        val mIntent = intent
-        if (mIntent.isActionSend) {
-            when (mIntent.type) {
-                null -> {
-                    xuiUtil
-                }
-                "text/plain" -> {
-                    saveText = mIntent.getStringExtra(Intent.EXTRA_TEXT)
-                    if (saveText.isNullOrEmpty()) {
-                        toast("空白文本，保存失败")
-                        finish()
-                    } else {
-                        loadingDialog = xuiUtil.materialDialogLoadingDialog("保存中", R.drawable.ic_save)
-                        val saveIntent = IntentUtil.getSaveFileByDocumentIntent("share.txt", "text/plain")
-                        startActivityForResult(saveIntent, 0)
+        mIntent = intent
+        try {
+            if (mIntent == null) {
+
+            } else if (mIntent!!.isActionSend) {
+                if (mIntent!!.type.isNullOrEmpty()) {
+                    ToastUtil.error("不支持分享这个类型的东西")
+                    finish()
+                } else {
+                    mimeType = mIntent!!.type!!
+                    when {
+                        mimeType == "text/plain" -> {
+                            val saveText = mIntent!!.getStringExtra(Intent.EXTRA_TEXT)
+                            if (saveText.isNullOrEmpty()) {
+                                ToastUtil.error("空白文本，保存失败")
+                                finish()
+                            } else {
+                                loadingDialog = xuiUtil.materialDialogLoadingDialog("保存中", R.drawable.ic_save)
+                                val time = DatetimeUtil.unixTimeStampMill().toString()
+                                val saveIntent = IntentUtil.getSaveFileByDocumentIntent("share-$time.txt", "text/plain")
+                                mimeType = "text"
+                                startActivityForResult(saveIntent, 0)
+                            }
+                        }
+                        mimeType.startsWith("image/") -> {
+                            val image = mIntent!!.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri
+                            if (image == null) {
+                                ToastUtil.error("空白图片，保存失败")
+                                finish()
+                            } else {
+                                loadingDialog = xuiUtil.materialDialogLoadingDialog("保存中", R.drawable.ic_save)
+                                val fileNameExtList = listOf("webp", "png", "jpeg")
+                                var fileNameExt = mimeType.remove("image/")
+                                if (fileNameExtList.hasNotChild(fileNameExt)) fileNameExt = "png"
+                                val time = DatetimeUtil.unixTimeStampMill().toString()
+                                val saveIntent = IntentUtil.getSaveFileByDocumentIntent("share-$time.$fileNameExt", mimeType)
+                                startActivityForResult(saveIntent, 0)
+                            }
+                        }
+                        else -> {
+                            ToastUtil.error("不支持分享这个类型的东西")
+                            finish()
+                        }
                     }
                 }
-                else -> {
-
-                }
+            } else {
+                ToastUtil.error("只能分享内容")
+                finish()
             }
-        } else {
-            toast("只能发送内容给我")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ToastUtil.error("发生未知错误")
             finish()
         }
     }
@@ -67,84 +101,122 @@ class Share2SaveActivity : Activity() {
             Activity.RESULT_OK -> {
                 when (requestCode) {
                     0 -> {
-                        // Save file
-                        if (resultData != null) {
-                            val uri = resultData.data
-                            when {
-                                uri == null -> {
-                                    ToastTintUtils.error("获取保存路径失败，即将退出")
-                                    finish()
-                                }
-                                saveText.isNullOrEmpty() -> {
-                                    ToastTintUtils.error("空白文本，不保存，即将退出")
-                                    finish()
-                                }
-                                else -> {
-                                    Logger.i("Uri: $uri")
-                                    val downloadPathList = listOf("content://com.android.providers.downloads.documents/", "content://downloads/")
-                                    if (uri.toString().startsWithList(downloadPathList)) {
-                                        ToastTintUtils.error("保存失败，不支持保存到'下载内容'里，请选择具体目录，即将退出")
-                                        finish()
-                                    } else {
-                                        var realUri: String? = null
-                                        try {
-                                            realUri = UriUtils.getFilePathByUri(this, uri)
-                                            if (realUri.isNullOrEmpty()) {
-                                                ToastTintUtils.error("真实保存路径空白，即将退出")
-                                                finish()
-                                            } else {
-                                                Logger.i("realUri: $realUri")
-                                                doAsync {
-                                                    val saveSu = FileUtil.saveFile(realUri, saveText!!)
-                                                    uiThread {
-                                                        loadingDialog?.recycle()
-                                                        if (saveSu) {
-                                                            xuiUtil.materialDialog("保存成功").apply {
-                                                                cancelListener {
-                                                                    finish()
-                                                                }
-                                                                onPositive { _, _ ->
-                                                                    finish()
-                                                                }
-                                                            }.show()
-                                                        } else {
-                                                            xuiUtil.materialDialog("保存失败").apply {
-                                                                cancelListener {
-                                                                    finish()
-                                                                }
-                                                                onPositive { _, _ ->
-                                                                    finish()
-                                                                }
-                                                            }.show()
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        } catch (e: Exception) {
-                                            e.printStackTrace()
-                                            Logger.i("realUri: (Exception)")
-                                            ToastTintUtils.error("获取真实保存路径失败，即将退出")
-                                            finish()
-                                        }
-                                    }
-                                }
-                            }
-                        } else {
-                            toast("未知代码，即将退出")
-                            finish()
-                        }
+                        afterCreateFile(resultData)
                     }
                     else -> {
-                        toast("未知代码，即将退出")
+                        ToastUtil.error("未知代码，即将退出")
                         finish()
                     }
                 }
             }
             Activity.RESULT_CANCELED -> {
-                toast(R.string.text_canceled_by_user)
+                ToastUtil.warning(R.string.text_canceled_by_user)
                 finish()
             }
         }
     }
 
+    private fun afterCreateFile(resultData: Intent?) {
+        // Save file
+        if (resultData != null) {
+            val uri = resultData.data
+            when (uri) {
+                null -> {
+                    ToastTintUtils.error("获取保存路径失败，即将退出")
+                    finish()
+                }
+                else -> {
+                    Logger.i("Uri: $uri")
+                    val downloadPathList = listOf("content://com.android.providers.downloads.documents/", "content://downloads/")
+                    if (uri.toString().startsWithList(downloadPathList)) {
+                        ToastTintUtils.error("保存失败，不支持保存到'下载内容'里，请选择具体目录，即将退出")
+                        finish()
+                    } else {
+                        val saveUri: String?
+                        try {
+                            saveUri = UriUtils.getFilePathByUri(this, uri)
+                            if (saveUri.isNullOrEmpty()) {
+                                ToastTintUtils.error("真实保存路径空白，即将退出")
+                                finish()
+                            } else {
+                                Logger.i("realUri: $saveUri")
+                                doAsync {
+                                    when {
+                                        mimeType == "text/plain" -> {
+                                            val saveText = mIntent!!.getStringExtra(Intent.EXTRA_TEXT)
+                                            if (saveText.isNullOrEmpty()) {
+                                                ToastTintUtils.error("空白文本，不保存，即将退出")
+                                                finish()
+
+                                            } else {
+                                                val saveSu = FileUtil.saveFile(saveUri, saveText)
+                                                uiThread {
+                                                    afterSave(saveSu)
+                                                }
+                                            }
+                                        }
+                                        mimeType.startsWith("image/") -> {
+                                            val image = mIntent!!.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as Uri
+                                            Logger.d("image:$image")
+                                            val inputStream = contentResolver.openInputStream(image)
+                                            val bmp = BitmapFactory.decodeStream(inputStream)
+                                            inputStream?.close()
+                                            //val imagePath = UriUtils.getFilePathByUri(this@Share2SaveActivity, image)
+                                            //Logger.d("imagePath:$imagePath")
+                                            //val saveSu = FileUtils.copyFile(imagePath, saveUri, true)
+                                            val format = when (mIntent!!.type!!.remove("image/")) {
+                                                "png" -> Bitmap.CompressFormat.PNG
+                                                "jpeg" -> Bitmap.CompressFormat.JPEG
+                                                "webp" -> Bitmap.CompressFormat.WEBP
+                                                else -> Bitmap.CompressFormat.PNG
+                                            }
+                                            val saveSu = BitmapUtils.saveBitmapToSDCard(bmp, saveUri, format, 100)
+                                            uiThread {
+                                                afterSave(saveSu)
+                                            }
+                                        }
+                                        else -> {
+                                            ToastUtil.error("发生未知错误")
+                                            finish()
+                                        }
+                                    }
+                                }
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            Logger.i("realUri: (Exception)")
+                            ToastTintUtils.error("获取真实保存路径失败，即将退出")
+                            finish()
+                        }
+                    }
+                }
+            }
+        } else {
+            ToastUtil.error("未知代码，即将退出")
+            finish()
+        }
+    }
+
+    private fun afterSave(su: Boolean) {
+        loadingDialog?.recycle()
+        if (su) {
+            xuiUtil.materialDialog("保存成功").apply {
+                cancelListener {
+                    finish()
+                }
+                onPositive { _, _ ->
+                    finish()
+                }
+            }.show()
+        } else {
+            xuiUtil.materialDialog("保存失败").apply {
+                cancelListener {
+                    finish()
+                }
+                onPositive { _, _ ->
+                    finish()
+                }
+            }.show()
+        }
+    }
 }

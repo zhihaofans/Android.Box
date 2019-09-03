32b4bfbd41ea2905c2e3c7845154f70912d4b2fc
zhihaofans <zhihaofans@hotmail.com>
2019-09-03 22:41:33 +0800

    Update

--- app/src/main/java/com/zhihaofans/androidbox/mod/ApiKeyMod.kt
+++ app/src/main/java/com/zhihaofans/androidbox/mod/ApiKeyMod.kt
@@ -7,8 +7,3 @@ package com.zhihaofans.androidbox.mod
  * @date: 2019-01-15 14:45
 
  */
-class ApiKeyMod {
-    companion object {
-        const val APIKEY_JUHE_WEIXIN = "583795c8a2d7460d0f11a9227fad005f"
-    }
-}
---
+++ app/src/main/java/com/zhihaofans/androidbox/mod/ApiMod.kt
@@ -0,0 +1,15 @@
+package com.zhihaofans.androidbox.mod
+
+/**
+ * 在此写用途
+
+ * @author: zhihaofans
+
+ * @date: 2019-09-03 22:24
+
+ */
+class ApiKeyMod {
+    companion object {
+        const val APIKEY_JUHE_WEIXIN = "583795c8a2d7460d0f11a9227fad005f"
+    }
+}
